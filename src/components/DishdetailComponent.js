c32c748607566e7e471b97ab9f7467504db037a6
Agravi Gupta <agravigupta1119@gmail.com>
2020-07-10 21:20:06 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -26,7 +26,8 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 
         handleSubmit(values){
             console.log('Current State is: ' + JSON.stringify(values));
-            alert('Current State is: ' + JSON.stringify(values));
+            //alert('Current State is: ' + JSON.stringify(values));
+            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
         }
 
         render(){
@@ -115,7 +116,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
             )
     }
 
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, dishId}) {
         if(comments != null)
             return(
                 <div className="col-12 col-md-5 m-1 mt-3">
@@ -132,7 +133,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                         )}
                         
                     </ul>
-                    <CommentForm />
+                    <CommentForm dishId={dishId} addComment={addComment} />
                 </div>  
                 
             )  
@@ -160,7 +161,8 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                 </div>
                     <div className="row">
                         <RenderDish dish={props.dish} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments} addComment={props.addComment}
+                        dishId={props.dish.id} />
                     </div>
                 </div>
             );
